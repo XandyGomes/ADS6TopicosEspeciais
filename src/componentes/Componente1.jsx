a83bead92708f9bbbff0061c95db7e28bd403c0a
XandyGomes <x3.computadores@hotmail.com>
2022-03-05 12:48:28 -0300

    Aula

---
+++ src/componentes/Componente1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// function func1() {
+//     return <h1>PRIMEIRO COMPONENTE</h1>
+// }
+// export default func1
+
+// export default () => <h1> Nosso componete Arrow</h1>
+
+// export default () => (5+7)
+
+export default (props) =>
+    <div>
+        <h1>{props.valor}</h1>
+        <h2>{props.soma}</h2>       
+    </div>
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Componente1 from './componentes/Componente1'
+// import { CompA, CompB as B} from "./componentes/DoisComponentes";
+// import MultiElementos from "./componentes/MultiElementos";
+// import { Teste1, Teste2, Teste3 } from "./componentes/MultiElementos";
+// import FamiliaGomes from "./componentes/FamiliaGomes";
+// import Familia from "./componentes/Familia";
+// import Nome from "./componentes/Nome";
+import ComponenteComFuncao from "./componentes/ComponenteComFuncao";
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        {/* <Componente1 valor="Olá pessoal" soma={3+4}/> */}
+        {/* <CompA valor = "Primeiro Componente"/> */}
+        {/* <B valor = "Segundo Componente"/> */}
+        {/* <MultiElementos/> */}
+        {/* <Teste1/>
+        <Teste2/>
+        <Teste3/> */}
+        {/* <FamiliaGomes/> */}
+        {/* <Familia sobrenome="Silva " endereco="Rua Tal " numero="nº 111 " cidade="Franca " estado="SP">
+                <Nome nome="Alexandre " />
+                <Nome nome="João "/>
+                <Nome nome="Sergio "/>
+        </Familia> */}
+        {/* <Familia>
+                <Nome nome="Joyce " sobrenome="Silva"/>
+                <Nome nome="Josimara " sobrenome="Silva"/>
+                <Nome nome="Janaina " sobrenome="Silva"/>
+        </Familia> */}
+        {/* <FamiliaGomes sobrenome="Gomes"/> */}
+        <ComponenteComFuncao/>
+    </div>
+, element
+)
